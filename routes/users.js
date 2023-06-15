65a11318f48f89917115dd8f2af5495086861e8e
Boualpso <cmtmoussa@gmail.com>
2023-06-15 04:58:22 +0300

    adding users

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,31 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/users', (req, res) =>{
-    res.send('Hello');
+const users = [{
+    firstname: "John",
+    lastname: "Doe",
+    age: 25
+},
+
+{
+    firstname: "Jane",
+    lastname: "Doe",
+    age: 24
+}];
+
+// all routes here are starting with /users
+router.get('/', (req, res) => {
+    console.log(users);
+
+    
+    res.send(users);
 });
 
+
+router.post('/', (req, res) => {
+
+    res.send(users);
+
+});
 export default router;
+

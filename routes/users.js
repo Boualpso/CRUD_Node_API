7af38fb13aea0ed5371a3466392e3a4330ec0fc3
Boualpso <cmtmoussa@gmail.com>
2023-06-17 04:36:27 +0300

    id added with uuid, deleting users based on id

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,12 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [{
-    firstname: "John",
-    lastname: "Doe",
-    age: 25
-},
-
-{
-    firstname: "Jane",
-    lastname: "Doe",
-    age: 24
-}];
+let users = [];
 
 // all routes here are starting with /users
 router.get('/', (req, res) => {
-    console.log(users);
-
-
     res.send(users);
 });
 
@@ -26,10 +14,31 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 
     const user = req.body;
-    //users.push()
-    res.send(`user with the username ${user.firstname} added to the database`);
+
+    const userId = uuidv4();
+
+    const userWithId = { ...user, id: userId }
+
+    users.push(userWithId);
+
+    res.send(`User with the username ${user.firstname} added to the database`);
 });
 
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    const findUser = users.find((user) => user.id ===id);
+    res.send(findUser);
+});
+
+router.delete ('/:id', (req, res) =>{
+    const { id } = req.params;
+
+    users = users.filter((user) => user,id != id);
+
+    res.send(`User with the ID ${id} deleted from database.`);
+});
 
 export default router;
 

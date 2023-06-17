783f2a76eb0bd439d97d6198efcd6337ece2e703
Boualpso <cmtmoussa@gmail.com>
2023-06-17 04:55:22 +0300

    patch users depending on id

--- routes/users.js
+++ routes/users.js
@@ -26,19 +26,36 @@ router.post('/', (req, res) => {
 
 
 router.get('/:id', (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
-    const findUser = users.find((user) => user.id ===id);
+    const findUser = users.find((user) => user.id === id);
     res.send(findUser);
 });
 
-router.delete ('/:id', (req, res) =>{
+router.delete('/:id', (req, res) => {
     const { id } = req.params;
 
-    users = users.filter((user) => user,id != id);
+    users = users.filter((user) => user, id != id);
 
     res.send(`User with the ID ${id} deleted from database.`);
 });
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstname, lastname, age } = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if (firstname) user.firstname = firstname;
+
+    if (lastname) user.lastname = lastname;
+
+    if (age) user.age = age;
+
+    res.send(`User with the ${id} has been updated.`);
+
+});
+
 
 export default router;
 
